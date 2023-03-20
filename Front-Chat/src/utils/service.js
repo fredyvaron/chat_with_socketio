9e87611c82351cc47e10c92ed3de78c4e5f5cc43
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-03-20 17:13:56 -0500

    bug de id in vercel

--- Front-Chat/src/utils/service.js
+++ Front-Chat/src/utils/service.js
@@ -4,7 +4,7 @@ export const local_url = import.meta.env.VITE_REACT_APP_URL_LOCAL;
 export const getidconversation = async (payload) => {
   console.log(payload);
   let { data } = await axios.post(`${local_url}/conversation/ids`, payload);
-  console.log(data)
+  console.log(data, "data de getidconversation in service")
   if (data) {
     return data.data.id;
   }
