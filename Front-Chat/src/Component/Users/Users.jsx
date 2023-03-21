18bd7407b09c11e9dc007d7bd6dca4948b444216
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-03-21 14:57:31 -0500

    loading user solution bug

--- Front-Chat/src/Component/Users/Users.jsx
+++ Front-Chat/src/Component/Users/Users.jsx
@@ -6,41 +6,35 @@ import User from "./User";
 
 export default function Users({ onUserSelection }) {
   const [loading, setLoading] = useState(true);
-  const [dataLoaded, setDataLoaded] = useState(false);
   const handleSelection = (userId) => {
     onUserSelection(userId);
     console.log(onUserSelection, "selected");
   };
-  console.log(loading, "loading")
+  console.log(loading, "loading");
 
   const dispatch = useDispatch();
   const users = useSelector((state) => state.data.users);
   useEffect(() => {
-    async function fetchData() {
-      await dispatch(get_all_users());
-      setDataLoaded(true);
-    }
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    if (dataLoaded) {
+    setLoading(true);
+    dispatch(get_all_users()).then(() => {
       setLoading(false);
-    }
-  }, [dataLoaded]);
+      console.log("se cambio el loading a false");
+    });
+  }, [dispatch]);
   return (
     <div className="h-screen overflow-hidden">
       <div className="h-5/6 overflow-y-auto">
         <div className="flex flex-col gap-2">
-
-          {  loading ? (
-          users?.map((user) => (
-            <div key={user.id} onClick={() => handleSelection(user.id)}>
-              <User User={user} />
-              <hr className="h-px  bg-gray-200 border-0 dark:bg-gray-700" />
-            </div>
-          )
-          )): (<Loading/>)}
+          {loading ? (
+            users?.map((user) => (
+              <div key={user.id} onClick={() => handleSelection(user.id)}>
+                <User User={user} />
+                <hr className="h-px  bg-gray-200 border-0 dark:bg-gray-700" />
+              </div>
+            ))
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
     </div>
