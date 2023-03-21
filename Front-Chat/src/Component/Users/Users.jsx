b8566d4206b633553425d67267e6f7c71eaefd1a
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-03-21 14:38:25 -0500

    loading user

--- Front-Chat/src/Component/Users/Users.jsx
+++ Front-Chat/src/Component/Users/Users.jsx
@@ -1,29 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { get_all_users } from "../../Redux/reducer";
+import Loading from "../Loading/Loading";
 import User from "./User";
 
 export default function Users({ onUserSelection }) {
+  const [loading, setLoading] = useState(true);
   const handleSelection = (userId) => {
     onUserSelection(userId);
-    console.log(onUserSelection,"selected")
+    console.log(onUserSelection, "selected");
   };
-  
+
   const dispatch = useDispatch();
   const users = useSelector((state) => state.data.users);
   useEffect(() => {
+    setLoading(true);
     dispatch(get_all_users());
+    setLoading(false);
   }, []);
   return (
     <div className="h-screen overflow-hidden">
       <div className="h-5/6 overflow-y-auto">
         <div className="flex flex-col gap-2">
-          {users?.map((user) => (
+
+          {  loading ? (
+          users?.map((user) => (
             <div key={user.id} onClick={() => handleSelection(user.id)}>
               <User User={user} />
               <hr className="h-px  bg-gray-200 border-0 dark:bg-gray-700" />
             </div>
-          ))}
+          )
+          )): (<Loading/>)}
         </div>
       </div>
     </div>
